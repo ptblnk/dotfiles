adbbbe5a25dfcd05fd4ef5e1710589d32e1ee2e6
ptblank <ptblank@pm.me>
2024-09-28 15:30:05 +1000

    add theme fix script

---
+++ theme-fix.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+gsettings set org.gnome.desktop.interface gtk-theme 'Adwaita-dark'
+gsettings set org.gnome.desktop.interface icon-theme 'elementary'
+gsettings set org.gnome.desktop.interface cursor-theme 'elementary'
+gsettings set org.gnome.desktop.interface font-name 'Roboto Slab 10'
+
+echo "Updated GTk themes"
+
